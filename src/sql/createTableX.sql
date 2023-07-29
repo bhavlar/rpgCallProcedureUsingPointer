07c732ebb166ca9aa7a43d3fa269a131477cbc3b
bhavlar <62497210+bhavlar@users.noreply.github.com>
2023-07-29 13:12:38 +0100

    Update createTableX.sql

--- src/sql/createTableX.sql
+++ src/sql/createTableX.sql
@@ -1,1 +1,12 @@
+/* create schema pocDB */
+create schema pocDB;
 
+/* create table tableX */
+create table pocDB.tableX (column01 char(10), column02 integer);
+
+/* Insert some rows into tableX */
+insert into pocDB.tableX 
+  values('aaa;,1),
+        ('bbb',2),
+        *'ccc',3)
+;
